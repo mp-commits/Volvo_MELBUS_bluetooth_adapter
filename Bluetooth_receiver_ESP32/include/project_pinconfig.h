74d58396e707ea67875ca1b21994d6de6e4b128d
Mikael Penttinen <mikael.penttinen3@gmail.com>
2022-04-03 12:31:12 +0300

    Update dac pins

--- Bluetooth_receiver_ESP32/include/project_pinconfig.h
+++ Bluetooth_receiver_ESP32/include/project_pinconfig.h
@@ -1,2 +1,5 @@
 #define ARDUINO_RESET_PIN 16
 #define ARDUINO_ENABLE_PIN 17
+#define DAC_BCLK_PIN 33
+#define DAC_DATA_PIN 25
+#define DAC_WSL_PIN 26
--- Bluetooth_receiver_ESP32/src/main.cpp
+++ Bluetooth_receiver_ESP32/src/main.cpp
@@ -9,6 +9,8 @@ BluetoothA2DPSink f_bluetoothSink;
 MediaControl f_mediaControl;
 MelbusController f_melbusController(&f_mediaControl);
 
+static void InitA2DPSink();
+
 void setup() {
     Serial.begin(115200);
     Serial.printf("Begin init bt receiver ver: %s\n", PROJECT_SOFTWARE_VERSION);
@@ -23,6 +25,25 @@ void setup() {
     f_mediaControl.SetDebug(true);
 
     Serial.printf("Starting bluetooth sink\n");
+    InitA2DPSink();
+
+    Serial.printf("Init Done!\n");
+}
+
+void loop() {
+    f_mediaControl.Task();
+    f_melbusController.Task();
+}
+
+void InitA2DPSink()
+{
+    i2s_pin_config_t my_pin_config = {
+        .bck_io_num = DAC_BCLK_PIN,
+        .ws_io_num = DAC_WSL_PIN,
+        .data_out_num = DAC_DATA_PIN,
+        .data_in_num = I2S_PIN_NO_CHANGE
+    };
+    f_bluetoothSink.set_pin_config(my_pin_config);
 
     static const i2s_config_t i2s_config = {
          .mode = (i2s_mode_t) (I2S_MODE_MASTER | I2S_MODE_TX),
@@ -37,12 +58,6 @@ void setup() {
          .tx_desc_auto_clear = true // avoiding noise in case of data unavailability
     };
     f_bluetoothSink.set_i2s_config(i2s_config);
+
     f_bluetoothSink.start("My test sink");
-
-    Serial.printf("Init Done!\n");
-}
-
-void loop() {
-    f_mediaControl.Task();
-    f_melbusController.Task();
 }
