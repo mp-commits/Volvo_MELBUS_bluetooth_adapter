524fff67ec953ab28edb1759c20089a5d08d483b
Mikael Penttinen <mikael.penttinen3@gmail.com>
2022-04-24 20:16:40 +0300

    Add melbus sniffer

---
+++ Arduino_melbus_sniffer/include/melbus.h
@@ -0,0 +1,40 @@
+/**
+ * @file melbus.h
+ * @author Mikael Penttinen (mikael.penttinen3@gmail.com)
+ * @brief Main header for the melbus component
+ * @version 0.1
+ * @date 2022-03-03
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#ifndef MELBUS_H
+#define MELBUS_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#define MELBUS_CLK_PIN 2
+#define MELBUS_DATA_PIN 3
+#define MELBUS_BUSY_PIN 4
+
+typedef uint8_t MELBUS_byte_t;
+
+bool MELBUS_Init();
+
+void MELBUS_BeginBusyInit(void);
+
+void MELBUS_SendByte(const MELBUS_byte_t byte);
+
+uint32_t MELBUS_RxBytesAvailable(void);
+
+MELBUS_byte_t MELBUS_ReadByte(void);
+
+bool MELBUS_ConfirmSync(void);
+
+bool MELBUS_IsBusy();
+
+uint32_t MELBUS_GetAvgFreq();
+
+#endif
---
+++ Arduino_melbus_sniffer/include/queue.h
@@ -0,0 +1,34 @@
+/**
+ * @file history.h
+ * @author Mikael Penttinen (mikael.penttinen3@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-03-04
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#ifndef QUEUE_H
+#define QUEUE_H
+
+#include "melbus.h"
+
+#define QUEUE_SIZE 128
+
+class MelbusQueue 
+{
+public:
+    MelbusQueue();
+    void Push(const MELBUS_byte_t byte);
+    MELBUS_byte_t Pop();
+    uint32_t Size();
+    uint32_t Count();
+private:
+    MELBUS_byte_t m_buffer[QUEUE_SIZE];
+    uint32_t m_readIndex;
+    uint32_t m_writeIndex;
+    uint32_t m_bytesAvailable;
+};
+
+#endif
